4f6a3650b5d65867948546aef9b46a723f021806
Matt Roumaya <matthewroumaya@gmail.com>
2023-03-19 11:00:41 -0400

    update logo and filter

--- util/subsetByDate.js
+++ util/subsetByDate.js
@@ -1,6 +1,7 @@
 export default function subsetByDate(data, pastOrFuture) {
   let now = new Date();
   if (pastOrFuture === "future") {
+    now = now - 86400000;
     return data.filter((el) => now < new Date(el.show_date));
   } else {
     return data.filter((el) => now > new Date(el.show_date));
