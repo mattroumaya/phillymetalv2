4dc7b2cb339fcba2a7c8b7d4d3940f981d5a50b2
Matt Roumaya <matthewroumaya@gmail.com>
2026-09-03 18:07:23 -0400

    add submitted by

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -12,6 +12,7 @@ import {
 } from "@coreui/react";
 
 const Homepage = () => {
+  const SUBMITTED_BY_SYSTEM = "SYSTEM";
   const [showFliers, setShowFliers] = useState(true);
   const [apiData, setApiData] = useState([]);
   const toggleFliers = () => setShowFliers(!showFliers);
@@ -76,6 +77,11 @@ const Homepage = () => {
                   width={250}
                   height={300}
                 />
+              )}
+              {dataItem.submitted_by !== SUBMITTED_BY_SYSTEM && (
+                <CListGroupItem className="submittedBy">
+                  Show submitted by: {dataItem.submitted_by}
+                </CListGroupItem>
               )}
             </CCardBody>
           </CCard>
